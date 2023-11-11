e74610426c805a710ab1990766f39c44857a14b6
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-11-11 06:58:19 -0500

    add openai completion

--- main.py
+++ main.py
@@ -26,13 +26,24 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
                                    text="I'm a bot please talk to me!")
 
 
+async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    messages.append({"role": "user", "context": update.message.text})
+    completion = openai.chat.completions.create(model="gpt-3.5-turbo",
+                                                messages=messages)
+    completion_answer = completion['choices'][0]['message']['content']
+    messages.append({"role": "assistant", "content": completion_answer})
 
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text=completion_answer)
 
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(tg_bot_token).build()
 
     start_handler = CommandHandler('start', start)
+    chat_handler = CommandHandler('chat', chat)
+
     application.add_handler(start_handler)
+    application.add_handler(chat_handler)
 
     application.run_polling()
