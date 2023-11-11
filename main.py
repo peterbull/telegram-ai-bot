b9436eec4cc65b46b6ebc41d045a69d9a3c49113
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-11-11 06:41:22 -0500

    add dice

--- main.py
+++ main.py
@@ -23,29 +23,18 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id,
                                    text="I'm a bot please talk to me!")
 
-class LabeledPrice:
-    def __init__(self, label: str, amount: float):
-        self.label = label
-        self.amount = amount
-
-price1 = LabeledPrice("price1", 1.01)
-price2 = LabeledPrice("price2", 2.04)
-labeled_prices = [price1, price2]
-
 async def scrumples(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_invoice(chat_id=update.effective_chat.id,
-                                   description="Mock invoice from Scrumples Inc.",
-                                   payload="You owe Scrumples some money",
-                                   currency="USD",
-                                   prices=labeled_prices
-                                   )
-    
+    await context.bot.send_dice(chat_id=update.effective_chat.id)
 
-    
+
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(tg_bot_token).build()
 
     start_handler = CommandHandler('start', start)
+    scrumple_handler = CommandHandler('scrumples', scrumples)
+
     application.add_handler(start_handler)
+    application.add_handler(scrumple_handler)
 
     application.run_polling()
