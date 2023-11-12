6d212577cd2820da755c80663ffc36c1a3e7b446
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-11-12 06:31:18 -0500

    update distance calcs for new wrapper

--- questions.py
+++ questions.py
@@ -2,6 +2,27 @@ import numpy as np
 import pandas as pd
 import openai
 import os
-from openai import distances_from_embeddings, OpenAI
+from openai import OpenAI
+from embedding_utils.distance import cosine_similarity
 
+client = OpenAI()
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+df = pd.read_csv('processed/embeddings.csv', index_col=0)
+df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
+
+
+
+
+def create_context(question, df, max_len=1800, size="ada"):
+    """
+    Create a context for a question by finding the most similar context from the dataframe
+    """
+
+    # Get the embeddings for a question
+    q_embeddings = client.embeddings.create(input=question, model='text-embedding-ada-002') \
+                   .data[0].embedding
+    
+    df['distances'] = df.embeddings.apply(lambda x: cosine_similarity(q_embeddings, x))
 
