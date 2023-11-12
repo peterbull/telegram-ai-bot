f8fd1bafbecad02b86af37e88633336171091c9e
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-11-12 14:10:01 -0500

    context working, but returns bad context

--- questions.py
+++ questions.py
@@ -24,5 +24,28 @@ def create_context(question, df, max_len=1800, size="ada"):
     q_embeddings = client.embeddings.create(input=question, model='text-embedding-ada-002') \
                    .data[0].embedding
     
-    df['distances'] = df.embeddings.apply(lambda x: cosine_similarity(q_embeddings, x))
+    df['distances'] = df.embeddings.apply(
+                      lambda x: cosine_similarity(np.array(q_embeddings), np.array(x)))
+
+    returns = []
+    cur_len = 0
+
+    # Sort by distance and add the text to the context until the context is too long
+    for i, row in df.sort_values('distances', ascending=True).iterrows():
+
+        # Add the length of the text to the current length
+        cur_len += row['n_tokens'] + 4
+
+        # If the context is too long, break
+        if cur_len > max_len:
+            break
+
+        # Else add it to the text that is being returned
+        returns.append(row["text"])
+
+    # Return the context
+    return "\n\n###\n\n".join(returns)
+
+
+create_context(question='What is CSS?', df=df)
 
